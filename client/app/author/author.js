b132c745e8143cda3df5b71479f1ed7ea9b8f073
arturisimo <arturisimo@gmail.com>
2015-08-28 15:04:03 +0200

    deploy commit

--- client/app/author/author.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('quizApp').config(function ($routeProvider) {
-    
-    $routeProvider.when('/author', {
-        templateUrl: 'app/author/author.html'
-    });
-
-});
